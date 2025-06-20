96cf66f8b91d5284b145d85162e148e0099eb911
Aakash <editsky597@gmail.com>
2025-06-20 12:56:55 +0530

    Intermediate Ques

---
+++ intermediateQues.sql
@@ -0,0 +1,71 @@
+# Intermediate:
+# Join the necessary tables to find the total quantity of each pizza category ordered;
+
+select * from pizzas;
+
+SELECT 
+    pizza_types.category, COUNT(orders_details.quantity)
+FROM
+    pizza_types
+        JOIN
+    pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN
+    orders_details ON orders_details.pizza_id = pizzas.pizza_id
+GROUP BY pizza_types.category;
+
+
+
+
+
+
+# Determine the distribution of orders by hour of the day.
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
